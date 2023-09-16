0bb88b4647c4b4cd38e8a574207b962820ba9e4c
jennifernguyen <jennifer.t.nguyen.02@gmail.com>
2023-09-16 15:10:43 -0400

    continuting styling the journaling page

--- frontend/src/pages/Journal.jsx
+++ frontend/src/pages/Journal.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import logo from '../logo.svg';
+import redx from '../redx.svg';
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
 
 export const Journal = () => {
   return (
@@ -15,34 +21,44 @@ export const Journal = () => {
       <div className="w-[696px] h-[46px] left-[180px] top-[167px] absolute text-black text-2xl font-normal font-['Raleway']">
         September 16, 2023
       </div>
-      <div className="w-[1048px] h-[385px] left-[195px] top-[354px] absolute bg-yellow-100 rounded-[10px] shadow" />
-      <div className="w-[696px] h-[46px] left-[223px] top-[388px] absolute text-black text-base font-normal font-['Raleway']">
-        Start typing here...
-      </div>
+
+      
+      <form onSubmit={handleSubmit} >
+            <input 
+                className="w-[1048px] h-[385px] left-[195px] top-[354px] absolute bg-yellow-100 rounded-[10px] shadow] absolute text-black text-base font-normal font-['Raleway']"
+                type="text" 
+                placeholder="Start typing here..." 
+                id="journal" 
+                name="journal" 
+                
+            />
+          <button type="submit">Submit</button>
+      </form>
+    
+     
       <div className="w-[696px] h-[46px] left-[180px] top-[293px] absolute text-black text-base font-normal font-['Raleway']">
         Minimum 100 words.
       </div>
       <div className="px-[7px] py-1 left-[1219px] top-[183px] absolute bg-black rounded-[5px] justify-start items-end gap-1 inline-flex">
-        <div className="w-10 text-center text-white text-sm font-medium font-['Inter'] leading-[21px]">Save</div>
+        <button className="w-10 text-center text-white text-sm font-medium font-['Raleway'] leading-[21px]">Save</button>
       </div>
       <div className="w-[33.75px] h-[27px] left-[1264px] top-[140px] absolute">
-        <div className="w-[33.75px] h-[27px] left-0 top-0 absolute bg-red-600 rounded-[5px]" />
-        <div className="w-[20.32px] h-[22.18px] left-[6.75px] top-[3.38px] absolute">
-          <div className="w-[6.51px] h-[23.62px] left-[4.95px] top-0 absolute origin-top-left rotate-[139.41deg] bg-white rounded-[0.50px]" />
-          <div className="w-[6.51px] h-[23.62px] left-[15.37px] top-0 absolute origin-top-left rotate-[40.59deg] bg-white rounded-[0.50px]" />
-        </div>
+      <button className="w-[33.75px] h-[27px] left-0 top-0 absolute">
+        <redx ClassName=""/>
+      </button>
+        
       </div>
       <div className="w-[76px] h-[31px] left-[1301px] top-[39px] absolute">
         <div className="w-[76px] h-[31px] left-0 top-0 absolute bg-white rounded-[5px] border border-black" />
-        <div className="w-[66px] left-[5px] top-[5px] absolute text-center text-black text-sm font-medium font-['Inter'] leading-[21px]">
+        <button className="w-[66px] left-[5px] top-[5px] absolute text-center text-black text-sm font-medium font-['Raleway'] leading-[21px]">
           Logout
-        </div>
+        </ button>
       </div>
       <div className="w-[358px] h-[47px] left-[12px] top-[23px] absolute">
         <div className="w-[289px] h-[19px] left-[69px] top-[7px] absolute text-amber-500 text-3xl font-bold font-['Raleway']">
           DJ Diary
         </div>
-        <img className="w-[55px] h-[47px] left-0 top-0 absolute" src="https://via.placeholder.com/55x47" />
+        <img className="w-[55px] h-[47px] left-0 top-0 absolute" src={logo} alt='DJ Diary Logo' />
       </div>
     </div>
   );
