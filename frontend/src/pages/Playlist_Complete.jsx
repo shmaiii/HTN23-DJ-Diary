cfc99465ad33654e15a6629d2470828910ec0832
jennifernguyen <jennifer.t.nguyen.02@gmail.com>
2023-09-16 21:30:30 -0400

    Fixed background colour issue and started mainPage

---
+++ frontend/src/pages/Playlist_Complete.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import logo from '../logo.svg';
+import { Header } from '../components/Header';
+
+export const Playlist_Complete = () => {
+	const navigate = useNavigate(); // Initialize navigate here
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<div className="w-fill h-screen bg-green-100 p-4">
+			<Header />
+		</div>
+	);
+};
